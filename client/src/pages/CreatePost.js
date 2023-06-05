9026ab3a666e7ba0fe2083d456ac7da255ffde57
Sanjin888 <sanjinkurtalija@gmail.com>
2023-06-05 22:49:25 +0200

    create post form

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage} from "formik";
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 function CreatePost() {
   return (
-    <div className='createPostContainer'></div>
-  )
+    <div className="createPostPage">
+      <Formik>
+        <Form>
+          <label>Title:</label>
+          <Field
+            id="inputCreatePost"
+            name="title"
+            placeholder="(Ex. Title...)"
+          />
+          <label>Title:</label>
+          <Field
+            id="inputCreatePost"
+            name="postText"
+            placeholder="(Ex. Post...)"
+          />
+          <label>Title:</label>
+          <Field
+            id="inputCreatePost"
+            name="username"
+            placeholder="(Ex. John123...)"
+          />
+
+          <button type="submit" >Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
