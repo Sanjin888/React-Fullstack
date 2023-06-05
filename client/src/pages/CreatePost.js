d8c7428e8383f1f2b0fbf96a0325b088987a3014
Sanjin888 <sanjinkurtalija@gmail.com>
2023-06-05 22:25:20 +0200

    added routing

---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage} from "formik";
+
+function CreatePost() {
+  return (
+    <div className='createPostContainer'></div>
+  )
+}
+
+export default CreatePost
