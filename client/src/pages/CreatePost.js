14902220d8bdd5159ab0c14e7234d8ed21dbd3cf
Sanjin888 <sanjinkurtalija@gmail.com>
2023-06-06 00:15:35 +0200

    individual post

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
   const initalValues = {
@@ -9,15 +11,21 @@ function CreatePost() {
     username: "",
   };
 
+  let navigate = useNavigate();
+  
   const validationSchema = Yup.object().shape({
-    title: Yup.string().required(),
+    title: Yup.string().required("You must input a Title!"),
     postText: Yup.string().required(),
     username: Yup.string().min(3).max(15).required(),
   });
 
   const onSubmit = (data) => {
-    console.log(data);
+    axios.post("http://localhost:3001/posts", data).then((response) => {
+      navigate("/")
+    });
   };
+
+  
   return (
     <div className="createPostPage">
       <Formik
