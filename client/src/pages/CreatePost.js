0d38e42090f50b7e249770f1ac98a924b92d00c8
Sanjin888 <sanjinkurtalija@gmail.com>
2023-06-05 23:01:22 +0200

    validation using Yup

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,31 +1,48 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function CreatePost() {
+
+  const initalValues= {
+    title: "",
+    postText: "",
+    username: ""
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required(),
+    postText: Yup.string().required(),
+    username:Yup.string().min(3).max(15).required()
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="createPostPage">
-      <Formik>
-        <Form>
+      <Formik initialValues={initalValues} onSubmit={onSubmit}> 
+        <Form className="formContainer">
           <label>Title:</label>
           <Field
             id="inputCreatePost"
             name="title"
             placeholder="(Ex. Title...)"
           />
-          <label>Title:</label>
+          <label>Post:</label>
           <Field
             id="inputCreatePost"
             name="postText"
             placeholder="(Ex. Post...)"
           />
-          <label>Title:</label>
+          <label>Username:</label>
           <Field
             id="inputCreatePost"
             name="username"
             placeholder="(Ex. John123...)"
           />
 
-          <button type="submit" >Create Post</button>
+          <button type="submit">Create Post</button>
         </Form>
       </Formik>
     </div>
